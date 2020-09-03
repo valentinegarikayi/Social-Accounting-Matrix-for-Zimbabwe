c9d2c07e61bcf910e606f8fd0b78b5e7ddcac2cf
Damien Farrell <farrell.damien@gmail.com>
2020-09-03 13:18:50 +0100

    r script

---
+++ zim_sam.R
@@ -0,0 +1,14 @@
+# SAM table analysis 
+
+library(ioanalysis)
+
+setwd('/home/damien/gitprojects/francis/Zim_SAM_Analysis/')
+
+Z = read.table('WP2018-139-Data-for-a-2013-Zimbabwe-SAM.csv', sep=',', row.names=1, header=TRUE)
+
+#create labels 
+n<-row.names(Z)
+#matrix(n)
+
+#create list of class InputOutput
+#as.inputoutput(Z, RS_label, X=)
