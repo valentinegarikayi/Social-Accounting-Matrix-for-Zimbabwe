7e929039f1a39448b0cd66005ae7615faa6cf43b
Damien Farrell <farrell.damien@gmail.com>
2021-07-05 23:22:42 +0100

    example R

--- zim_sam.R
+++ zim_sam.R
@@ -1,4 +1,5 @@
 # SAM table analysis 
+# https://rdrr.io/cran/ioanalysis/man/as.inputoutput.html
 
 library(ioanalysis)
 
@@ -6,8 +7,10 @@ setwd('/home/damien/gitprojects/francis/Zim_SAM_Analysis/')
 
 Z = read.table('WP2018-139-Data-for-a-2013-Zimbabwe-SAM.csv', sep=',', row.names=1, header=TRUE)
 
-#create labels 
+#create labels
 n<-row.names(Z)
+n
+
 #matrix(n)
 
 #create list of class InputOutput
